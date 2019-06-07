6f924b2636af223b029182b85781afd15ef19c97
Pablo384 <pabloreinoso0@gmail.com>
2019-06-06 23:16:49 -0400

    correccion XD

--- api/controllers/persona/create.js
+++ api/controllers/persona/create.js
@@ -66,21 +66,21 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    const objVerbo = await Persona.create(inputs).fetch();
-
+    
     if (inputs.esEstudiante) {
-
+      
       let exist = await Persona.find({matricula: inputs.matricula, estado: 'A'});
       let exist02 = await Persona.find({carnet: inputs.carnet, estado: 'A'});
       if (exist.length > 0) {
         return this.res.status(401).json({ info : false, message : 'Ya existe estudiante con esa matricula'});
-
+        
       }
       if (exist02.length > 0) {
         return this.res.status(401).json({ info : false, message : 'Ya existe estudiante con ese carnet'});
-
+        
       }
     }
+    const objVerbo = await Persona.create(inputs).fetch();
     if (objVerbo.length !== 0) {
       return exits.success({ info : true, message : 'Persona registrada correctamente.', data:objVerbo});
     }
