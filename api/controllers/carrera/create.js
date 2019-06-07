2bd5afd8ae8a94d68b7ef11f66bef0d045528558
Pablo384 <pabloreinoso0@gmail.com>
2019-06-06 22:28:18 -0400

    correcion

--- api/controllers/carrera/create.js
+++ api/controllers/carrera/create.js
@@ -30,24 +30,24 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-  /*
-      return this.res.status(500).json({
-        error: true,
-        status: 401,
-        message: 'Usuario no existe'
-      });
-      return exits.success({ error: false, user: us, token: token, message: 'Correcto' });
-      { info : true, message : "Carrera registrada correctamente."}
-      */
-    const unique = await Carrera.find({nombre: inputs.nombre, estado: 'A'});
+    /*
+        return this.res.status(500).json({
+          error: true,
+          status: 401,
+          message: 'Usuario no existe'
+        });
+        return exits.success({ error: false, user: us, token: token, message: 'Correcto' });
+        { info : true, message : "Carrera registrada correctamente."}
+        */
+    const unique = await Carrera.find({ nombre: inputs.nombre, estado: 'A' });
     if (unique.length > 0) {
-      return this.res.status(401).json({ info : false, message : 'Nombre de esta carrera ya existe'});
+      return this.res.status(401).json({ info: false, message: 'Nombre de esta carrera ya existe' });
     }
     const objVerbo = await Carrera.create(inputs).fetch();
-    if (objVerbo.length !== 0) {
-      return exits.success({ info : true, message : 'Carrera registrada correctamente.'});
+    if (objVerbo && objVerbo.length !== 0) {
+      return exits.success({ info: true, message: 'Carrera registrada correctamente.' });
     }
-    return this.res.status(401).json({ info : false, message : 'Error al registrar carrera.'});
+    return this.res.status(401).json({ info: false, message: 'Error al registrar carrera.' });
 
   }
 
--- api/controllers/persona/create.js
+++ api/controllers/persona/create.js
@@ -67,6 +67,20 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     const objVerbo = await Persona.create(inputs).fetch();
+
+    if (inputs.esEstudiante) {
+
+      let exist = await Persona.find({matricula: inputs.matricula, estado: 'A'});
+      let exist02 = await Persona.find({carnet: inputs.carnet, estado: 'A'});
+      if (exist.length > 0) {
+        return this.res.status(401).json({ info : false, message : 'Ya existe estudiante con esa matricula'});
+
+      }
+      if (exist02.length > 0) {
+        return this.res.status(401).json({ info : false, message : 'Ya existe estudiante con ese carnet'});
+
+      }
+    }
     if (objVerbo.length !== 0) {
       return exits.success({ info : true, message : 'Persona registrada correctamente.', data:objVerbo});
     }
